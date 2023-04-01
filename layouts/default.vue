09753e697324b8479c7a094fe19bb3ce246aa0f4
hlp <865225435@qq.com>
2023-04-01 16:28:39 +0800

    build: naive-ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,7 @@
         alt="avatar"
       />
     </nav>
+    <slot />
   </div>
 </template>
 
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    <NButton>hello</NButton>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
