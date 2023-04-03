2711d1b9bf1778e0e11f1a3dc69e5af59ba680a0
hlp <865225435@qq.com>
2023-04-03 18:33:55 +0800

    feat: server api

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,28 @@
-<script setup lang='ts'>
+<script setup lang="ts">
+type ListType = {
+    id: string;
+    title: any;
+    date: Date | string;
+}
 
+const list: ListType[] = await $fetch('/api/post')
+// const list = listData.map((data) => {
+//   data.date = (data.date as Date).getTime() + ''
+//   return data
+// })
 </script>
 
 <template>
   <div>
-    <NButton>hello</NButton>
+    <n-card
+      v-for="(data, index) in list"
+      :key="index"
+      :title="data.date"
+      hoverable
+    >
+      {{ data.title }}
+    </n-card>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
